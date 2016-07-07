b4fe75c993b2cbf3e4a5bd3295141f2377e6b7b8
YAN7 <904394373@qq.com>
2016-07-07 23:56:19 +0800

    index页面布局完成 -- gh-pages仓储

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*
+* @Author: yan7
+* @Date:   2016-07-07 08:02:32
+* @Last Modified by:   apple
+* @Last Modified time: 2016-07-07 08:05:24
+*/
+
+'use strict';
+$(function() {
+	// 拼接字符串？
+	var newLi = '';
+	newLi += "<li>";
+
+
+
+})
+
+
